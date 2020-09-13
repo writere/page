b801fa44cb5599d407f62b4c89a4add187c857c4
liuhao <3480693679@qq.com>
2020-09-13 21:15:14 +0800

    test1

--- app/src/main/java/com/futrue/page/WelcomeActivity.java
+++ app/src/main/java/com/futrue/page/WelcomeActivity.java
@@ -1,11 +1,14 @@
 package com.futrue.page;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.futrue.page.follower.SharedPreferencesManager;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -21,7 +24,31 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        skip();
     }
+
+    /*
+     * 函数：skip()
+     * 功能：检查初始化跳转情况
+     */
+    private void skip(){
+        SharedPreferencesManager sharedPreferencesManager = new SharedPreferencesManager(getBaseContext(),"skip", Context.MODE_PRIVATE);
+        boolean isFirst = sharedPreferencesManager.get("isFirst", true);
+        boolean isLogin = sharedPreferencesManager.get("isLogin", false);
+        if (!isFirst){
+            if (isLogin){
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                this.finish();
+            } else {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                this.finish();
+            }
+        }
+    }
+
+
 
 
 
--- app/src/main/java/com/futrue/page/follower/SharedPreferencesManager.java
+++ app/src/main/java/com/futrue/page/follower/SharedPreferencesManager.java
@@ -1,6 +1,41 @@
 package com.futrue.page.follower;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SharedPreferencesManager {
 
+    private SharedPreferences sharedPreferences;
+
+    public SharedPreferencesManager(Context context, String name, int mode){
+        sharedPreferences = context.getSharedPreferences(name, mode);
+    }
+
+    public void put(String name, String content){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(name, content);
+        editor.commit();
+    }
+
+    public void put(String name, boolean content){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(name, content);
+        editor.commit();
+    }
+
+
+    public void put(String name, int content){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(name, content);
+        editor.commit();
+    }
+
+    public String get(String name, String defValue){
+        return sharedPreferences.getString("name",defValue);
+    }
+
+    public boolean get(String name, boolean defValue){
+        return sharedPreferences.getBoolean("name", defValue);
+    }
 
 }
